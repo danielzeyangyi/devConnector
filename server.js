12197246f37e6c937392cfe6f27b2427839964bc
Zeyang Yi <danielzeyangyi@gmail.com>
2019-04-29 19:48:43 -0400

    DB set up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect to DB
+connectDB();
+
 app.get("/", (req, res) => res.send("API running now"));
 
 const PORT = process.env.PORT || 5000;
