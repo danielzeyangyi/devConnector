76fe773ab260d783c20962e6cb84d8f837bba7bc
Zeyang Yi <danielzeyangyi@gmail.com>
2019-05-08 02:35:11 -0400

    load user set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -6,13 +6,23 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 
+import './App.css';
+
 //REDUX
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-import './App.css';
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
@@ -30,6 +40,6 @@ function App() {
       </Router>
     </Provider>
   );
-}
+};
 
 export default App;
