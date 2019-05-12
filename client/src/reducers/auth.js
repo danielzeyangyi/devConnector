04264e6781863233199ef82f59824846b10fa917
Zeyang Yi <danielzeyangyi@gmail.com>
2019-05-11 21:53:40 -0400

    login logout navbar links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,10 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -25,6 +28,7 @@ export default function(state = initialState, actions) {
       };
 
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -35,6 +39,8 @@ export default function(state = initialState, actions) {
 
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
