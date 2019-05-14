602f1f7ec726fd52167a4844f6014e38b7f56a2b
Zeyang Yi <danielzeyangyi@gmail.com>
2019-05-14 19:08:13 -0400

    create profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
@@ -6,9 +7,8 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
-
-import './App.css';
 
 //REDUX
 import { Provider } from 'react-redux';
@@ -37,6 +37,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
