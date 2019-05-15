9d1b2270eda85ae70cc6a5a51d5f6e325094e05a
Zeyang Yi <danielzeyangyi@gmail.com>
2019-05-15 01:57:46 -0400

    added education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 //REDUX
 import { Provider } from 'react-redux';
@@ -47,6 +49,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
